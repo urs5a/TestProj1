00839779fd38a26c647dd62bcae159d2aa2c8bcd
Ursula Oddo-White <uoddowhi@stevens.edu>
2024-04-06 12:46:47 -0400

    step 1 done, all works

--- run_proj.py
+++ run_proj.py
@@ -1,8 +1,7 @@
 #pasted from website
-import researchos as ros #switched back to researchos
+import ResearchOS as ros #originally was researchos
 
 ros.DBInitializer() 
 
-#went to power shell and re ran the pip instal line in hop eof re downloading researchos and fixing the variable rpoblem
-#now everything is yellow and this file is giving the ModuleNotFoundError
-#I tried sayign researchos as the original line from the webcite suggested but it didn't help
+#STEP 1 DONE
+#all worked
