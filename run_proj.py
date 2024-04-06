e702bf14c37c804da85d245d3150ebb3d0a4a0af
Ursula Oddo-White <uoddowhi@stevens.edu>
2024-04-06 14:45:50 -0400

    step 2 DONE, works, step 3 started

--- run_proj.py
+++ run_proj.py
@@ -1,3 +1,4 @@
+#STEP 1 below
 #pasted from website
 import ResearchOS as ros #originally was researchos
 
@@ -5,3 +6,19 @@ ros.DBInitializer()
 
 #STEP 1 DONE
 #all worked
+
+#STEP 2
+# should specify HOW we are creating dummy_data.csv
+# I'm left wondering if it matters how I make it, do I just make my own excel doc?
+# might seem obvious but I would say something like "you can make this csv file using excel or what ever you normally use"
+# how do I get it into the project? do I just drag it into the project folder? 
+# if uploading the dummy data is an another step I would also say something like "this step is just to create the data, we will connect it to our test projet in the next step"
+
+# i liked the text the end explaining what the fake data means, again seems obvious but it makes it easier to follow along
+
+#i made my dummy_data by saving excel as a CSV (comma deliminated)
+
+#STEP 3
+#should specify to create dataset.py inside of new research_objects folder
+
+#typo: Data Objects are custom defined IN each project
